fb8b187519935d27f0e05547784a315cafc2a095
Skanda Koppula <skanda.koppula@gmail.com>
2014-06-14 22:15:41 -0400

    removing big files

--- problem1.py
+++ problem1.py
@@ -4,21 +4,44 @@ lines = infile.readlines()
 num_test_cases = int(lines[0].strip())
 index = 1
 
+max_score = -1
+max_sentence = ''
+outfile = open('s1_ans.out', 'w')
+
 while index < len(lines):
 	index += 1 #skip blank line
 	num_sents = int(lines[index].strip())
 	index += 1 #push reader index to next line
-	
+
 	for i in range(index, index+num_sents):
 		sentence = lines[index].strip()
 		score = detScore(sentence)
-		words = lines.split()
-		in_asterick = 
-		tipe = [None]*len(words)
-		for word in words:
+		if score > max_score:
+			max_sentence = sentence
+			max_score = score
+		
+	outfile.write(str(max_score) + '\n')
 
+import re
 
 def detScore(s):
+	words = lines.split()
+	n = len(words)
+	in_asterick = [False]*len(words)
+	typ = [None]*len(words)
+	in_ast_space = False
+	for i, word in enumerate(words):
+		begin = True if word[0] =='*' else False
+		end = True if word[-1] == '*' else False
+		if begin:
+			in_asterick = in_asterick[:i].append([not in_asterick[i]]*(n-i))
+		if end:
+			in_asterick = in_asterick[:i+1].append([not in_asterick[i]]*(n-i-1))
+
+	for word in enumerate(words):
+		if
+			
+		
 		
 
 
