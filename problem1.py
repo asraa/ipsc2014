25451f8adaadd30b6049f34f619e34ea23b2b32a
Skanda Koppula <skanda.koppula@gmail.com>
2014-06-14 21:26:18 -0400

    hello

--- problem1.py
+++ problem1.py
@@ -1,6 +1,25 @@
-infile = open('sa1.in', 'r')
+infile = open('s1.in', 'r')
 lines = infile.readlines()
 
-read_lines_counter = 0
 num_test_cases = int(lines[0].strip())
+index = 1
+
+while index < len(lines):
+	index += 1 #skip blank line
+	num_sents = int(lines[index].strip())
+	index += 1 #push reader index to next line
+	
+	for i in range(index, index+num_sents):
+		sentence = lines[index].strip()
+		score = detScore(sentence)
+		words = lines.split()
+		in_asterick = 
+		tipe = [None]*len(words)
+		for word in words:
+
+
+def detScore(s):
+		
+
+
 print num_test_cases
